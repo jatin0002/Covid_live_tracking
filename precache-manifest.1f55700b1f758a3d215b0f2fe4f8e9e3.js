453bc48df9fcd23bfe56a80d02e666cc8488253f
jatin0002 <56964396+jatin0002@users.noreply.github.com>
2020-04-09 04:03:37 +0530

    Updates

---
+++ precache-manifest.1f55700b1f758a3d215b0f2fe4f8e9e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a57445099b407cdb5dab4d9c07153ce",
+    "url": "/Covid_live_tracking/index.html"
+  },
+  {
+    "revision": "4889c54205fada8cb6f0",
+    "url": "/Covid_live_tracking/static/css/main.028e6c18.chunk.css"
+  },
+  {
+    "revision": "e28aa2cf903aa4b6ac9f",
+    "url": "/Covid_live_tracking/static/js/2.7c0035f7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid_live_tracking/static/js/2.7c0035f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4889c54205fada8cb6f0",
+    "url": "/Covid_live_tracking/static/js/main.5a832a2e.chunk.js"
+  },
+  {
+    "revision": "8f302cbb833b2e6c89c9",
+    "url": "/Covid_live_tracking/static/js/runtime-main.180c9faf.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid_live_tracking/static/media/image.d7265326.png"
+  }
+]);
